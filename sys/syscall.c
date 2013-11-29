df0f2a60ae6f1b4939e5ae16e681891227cb660a
zhtlancer <zhtlancer@gmail.com>
2013-11-29 09:46:46 -0500

    Finished implementation of write syscall.

--- sys/syscall.c
+++ sys/syscall.c
@@ -1,6 +1,8 @@
 #include <sys/msr.h>
 #include <sys/syscall.h>
 #include <sys/sched.h>
+#include <sys/k_stdio.h>
+#include <sys/fs.h>
 
 #define SYSCALL_CS	0x08
 #define SYSRET_CS	0x1B
@@ -8,6 +10,16 @@
 #define EFER_SCE	0x0000000000000001UL
 
 #define RFLAGS_IF	0x200
+
+#define syscall_error(fmt, ...)	\
+	k_printf(1, "<SYSCALL> [%s (%s:%d)] " fmt, __func__, __FILE__, __LINE__, ## __VA_ARGS__)
+
+#if DEBUG_SYSCALL
+#define syscall_db(fmt, ...)	\
+	k_printf(1, "<SYSCALL DEBUG> [%s (%s:%d)] " fmt, __func__, __FILE__, __LINE__, ## __VA_ARGS__)
+#else
+#define syscall_db(fmt, ...)
+#endif
 
 int syscall_init(void)
 {
@@ -34,8 +46,25 @@ int syscall_init(void)
 	return 0;
 }
 
+uint64_t sys_write(struct context *ctx)
+{
+	struct file *file = current->files[ctx->rdi];
+	if (!file->writeable || file->f_ops->write == NULL)
+		return 0;
+	return file->f_ops->write(file, (void *)ctx->rsi, ctx->rdx);
+}
+
 uint64_t syscall_common(struct context *ctx)
 {
+	uint64_t syscall_no = ctx->rax;
+
+	switch (syscall_no) {
+	case SYS_write:
+		return sys_write(ctx);
+	default:
+		syscall_error("Undefined syscall number (0x%x)\n", syscall_no);
+		break;
+	}
 	return 0;
 }
 

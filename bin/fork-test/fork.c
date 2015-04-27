09bcecc5d053530db0366c0d1226570fb429aead
Tao Zhang <zhtlancer@gmail.com>
2015-04-27 18:46:40 -0400

    Improve fork-test

--- bin/fork-test/fork.c
+++ bin/fork-test/fork.c
@@ -14,7 +14,10 @@ int main(int argc, char *argv[], char *envp[])
 	if (pid == 0) {
 		printf("\tI'm child, pid=%d\n", getpid());
 	} else {
-		printf("\tI'm parent, child id=%d\n", pid);
+		int status;
+		printf("\tI'm parent, child id=%d, waiting for child to exit...\n", pid);
+		wait(&status);
+		printf("\tChild exited with status (%d).\n", status);
 	}
 }
 /* vim: set ts=4 sw=0 tw=0 noet : */
